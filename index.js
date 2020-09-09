d6759de31c1e2494d3490872dd95e46350d94fe1
Invisioncoregit <mafnas_conan@invisioncore.com>
2020-09-09 00:55:59 -0400

    init commit

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express'); 
-const users = require('./users'); 
+const express = require('express');
+const users = require('./users');
 
-const app = express(); 
+const app = express();
 
-app.listen(process.env.PORT || 8081, () => {
-    console.log(`Server is running on port`); 
-})
+app.listen(8080, () => {
+  console.log(`Server is running on port 8080`);
+});
 
 app.get('/', (_, res) => {
-    res.send('Hello'); 
-})
+  res.send('Hello');
+});
 
 app.get('/users', (_, res) => {
-    res.json(users); 
-})
+  res.json(users);
+});
 
 app.get(`/users/:userId`, (req, res) => {
-    if (req.params.userId === undefined || req.params.userId === null) {
-         res.console.error("undefined");
-     }
-    const selectedUser = users.filter((user) => user.userId === Number(req.params.userId)); 
+  if (req.params.userId === undefined || req.params.userId === null) {
+    res.console.error('undefined');
+  }
+  const selectedUser = users.filter(
+    (user) => user.userId === Number(req.params.userId)
+  );
 
-    res.json(selectedUser); 
-})
-
-
+  res.json(selectedUser);
+});
